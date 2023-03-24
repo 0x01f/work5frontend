a9dcf6dcead9142dfd8b60c27953f54b82a1e5ce
Vyacheslav Starovoytov <vyacheslav.starovoytov@brotherhood.software>
2023-03-24 20:30:41 +0300

    004

--- js/common.js
+++ js/common.js
@@ -1,1 +1,11 @@
-// $('body').hide()
+const myButton = document.getElementById("myButton");
+const myPopup = document.getElementById("myPopup");
+const closePopup = document.getElementById("closePopup");
+
+myButton.addEventListener("click", () => {
+  myPopup.classList.add("show");
+});
+
+closePopup.addEventListener("click", () => {
+  myPopup.classList.remove("show");
+});
